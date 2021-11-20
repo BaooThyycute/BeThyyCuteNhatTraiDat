8678b35012ae232bdeeff31acda5526f2177619d
BaooThyycute <94509204+BaooThyycute@users.noreply.github.com>
2021-11-20 17:20:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Bé Thyy cute ơi",
+    introTitle: 'bé Thyy cute ơi!',
+    introDesc: `Có điều này muốn nói với bé`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Bé có yêu anh không 🥰',
+    desc: 'Trả lời thật lòng nha',
+    btnYes: 'yêu ơi là yêu😘😘',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'sao lại yêu thế <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu quá không biết phải nói sao <33333333',
+    mess: 'Anh biết mà 🥰. Yêu bé Thyy nhất 😘😘',
+    messDesc: 'vậy thì mau rep ib thôi nàooooo',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.messenger.com/t/100038502994968' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
